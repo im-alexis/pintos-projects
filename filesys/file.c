e4862a5d8ffa0f6e0f047dab6c9a776e440c3da0
Guadalupe-Melendez <lupitamelendez898@yahoo.com>
2023-10-05 00:13:00 +0000

    Didn't push everything

--- filesys/file.c
+++ filesys/file.c
@@ -37,7 +37,7 @@ void
 file_close(struct file *file)
 {
     if (file != NULL) {
-        // file_allow_write(file); // commented this out because TA told us to.
+        file_allow_write(file); // commented this out because TA told us to. WE WERE LIED TO :|
         inode_close(file->inode);
         free(file);
     }
